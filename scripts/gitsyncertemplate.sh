016e9fd7a33139a6d5cf6fbc7691b3a55cc31fbf
MRHwick <MatthewRHardwick@gmail.com>
2020-06-30 21:52:01 -0500

    add encrypted obsidian journal

--- scripts/gitsyncertemplate.sh
+++ scripts/gitsyncertemplate.sh
@@ -4,7 +4,7 @@ date
 echo " syncing journal repo to github.."
 echo "\n"
 git pull origin master
-git add jrnl.text
+git add journal
 git commit -m "Adding new journal data"
 git push origin master
 echo "\n\n"
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,3 @@
-brew install jrnl
-
-# Add the config
-mkdir -p ~/.config/jrnl
-cp config.yaml ~/.config/jrnl/jrnl.yaml
-
-# substitute the default jrnl with the file from this repo
-currentjrnl=$(pwd)"/jrnl.text"
-sed -i.bu "s|PLACEHOLDER|${currentjrnl}|g" ~/.config/jrnl/jrnl.yaml
-
 # substitue the syncer repo with this repo
 repo=$(pwd)
 cp scripts/synccrontemplate scripts/cron
@@ -15,5 +5,3 @@ sed -i.bu "s|PLACEHOLDER|${repo}|g" scripts/cron
 cp scripts/gitsyncertemplate.sh scripts/gitsyncer.sh
 sed -i.bu "s|PLACEHOLDER|${repo}|g" scripts/gitsyncer.sh
 crontab scripts/cron
-
-security add-generic-password -a default -s jrnl -U -w

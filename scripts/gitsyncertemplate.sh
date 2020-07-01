eeb8bb56182d50488c3d9dc6a0d8e84c7752d5fb
MRHwick <MatthewRHardwick@gmail.com>
2020-06-30 23:38:53 -0500

    change syncer template

--- scripts/gitsyncertemplate.sh
+++ scripts/gitsyncertemplate.sh
@@ -1,10 +1,10 @@
 cd PLACEHOLDER
+export PATH="/usr/local/bin:$PATH"
 
 date
 echo " syncing journal repo to github.."
-echo "\n"
 git pull origin master
 git add journal
 git commit -m "Adding new journal data"
 git push origin master
-echo "\n\n"
+echo "\n"
